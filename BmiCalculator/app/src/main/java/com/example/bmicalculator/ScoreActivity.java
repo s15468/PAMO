0c289a1bc843baf914650911451f7c1a02035f4e
Julian Chodorowski <xjulasx@gmail.com>
2021-04-02 23:19:00 +0200

    Added dishes proposition

--- BmiCalculator/app/src/main/java/com/example/bmicalculator/ScoreActivity.java
+++ BmiCalculator/app/src/main/java/com/example/bmicalculator/ScoreActivity.java
@@ -1,6 +1,10 @@
 package com.example.bmicalculator;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,8 @@ public class ScoreActivity extends AppCompatActivity
 {
     private TextView yourScore;
     private TextView yourBMR;
+    private Button openRecipeButton;
+    private String url;
     private Float weight;
     private Float height;
     private String sex;
@@ -20,6 +26,7 @@ public class ScoreActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
 
+        openRecipeButton = (Button) findViewById(R.id.openRecipe);
         yourScore = (TextView) findViewById(R.id.scoreText);
         yourBMR = (TextView) findViewById(R.id.bmrText);
         Bundle extras = getIntent().getExtras();
@@ -34,6 +41,17 @@ public class ScoreActivity extends AppCompatActivity
 
         displayBMI(calculateBMI());
         displayBMR(calculateBMR());
+
+        openRecipeButton = (Button) findViewById(R.id.openRecipe);
+        openRecipeButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(browserIntent);
+            }
+        });
     }
 
     private float calculateBMR()
@@ -73,6 +91,13 @@ public class ScoreActivity extends AppCompatActivity
             bmiLabel = "Obese Class II";
         else
             bmiLabel = "Obese Class III";
+
+        if (bmi < 18.5f)
+            url = "https://www.100daysofrealfood.com/loaded-black-bean-burgers/";
+        else if (bmi >= 18.5f && bmi <= 25f)
+            url = "https://www.100daysofrealfood.com/chicken-pesto-pizza/";
+        else if (bmi > 25f)
+            url = "https://www.100daysofrealfood.com/green-smoothie-recipe/";
 
         yourScore.setText(bmiLabel);
     }
